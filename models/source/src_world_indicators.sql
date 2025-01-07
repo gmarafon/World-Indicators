f5529d46d48b34a773499f3ac1626c48e72742d2
Gabriel Marafon <gfmarafon@gmail.com>
2025-01-07 16:11:13 -0300

    Add dbt configuration and initial SQL models for world indicators

---
+++ models/source/src_world_indicators.sql
@@ -0,0 +1,2 @@
+SELECT *
+FROM `world-indicators-447017.raw_data.world_bank_data`
---
+++ models/staging/stg_world_indicators.sql
@@ -0,0 +1,17 @@
+SELECT
+  `Country`,
+  `Economy` as `Economy ID`,
+  `Series`,
+  `Series ID`,
+  `YR2014` as '2014',
+  `YR2015` as '2015',
+  `YR2016` as '2016',
+  `YR2017` as '2017',
+  `YR2018` as '2018',
+  `YR2019` as '2019',
+  `YR2020` as '2020',
+  `YR2021` as '2021',
+  `YR2022` as '2022',
+  `YR2023` as '2023'
+FROM
+  `world-indicators-447017.dbt_world_indicators.src_world_indicators` ;
