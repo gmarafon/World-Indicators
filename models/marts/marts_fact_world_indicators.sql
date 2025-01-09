835df174959122f60fd4816d2dff20384febd917
Gabriel Marafon <gfmarafon@gmail.com>
2025-01-09 19:19:54 -0300

    Explore data for transformations and fact table SQL adjustments

--- models/marts/marts_fact_world_indicators.sql
+++ models/marts/marts_fact_world_indicators.sql
@@ -2,8 +2,10 @@ WITH unpivoted AS (
   SELECT 
     `Economy ID`,
     `Series ID`,
+    `Series`,
     CAST(Year AS INT64) as Year, #to make sure the value is a number
-    CAST(Value AS FLOAT64) as Value #to make sure the value is a number
+    CAST(Value AS FLOAT64) as Value, #to make sure the value is a number
+    REGEXP_EXTRACT(`Series`, r'.*\((.*?)\)') as `Measurement Unity`
   FROM 
     `world-indicators-447017.dbt_world_indicators.stg_world_indicators`
   UNPIVOT(
@@ -17,6 +19,9 @@ WITH unpivoted AS (
 SELECT
   `Economy ID`,
   `Series ID`,
+  `Series`,
+  `Measurement Unity`,
+  `Measurement Unity` as `Unity Type`,
   Year,
   Value
 FROM 
